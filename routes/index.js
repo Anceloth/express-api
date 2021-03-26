c0aac2a0fefb4f37cac7511d038edf9adf4ac071
JonathanM <jonathan.marin.c@gmail.com>
2021-03-26 17:04:00 -0500

    manipulating pug template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Practice' });
+  res.render('index', 
+    { title: 'Express Practice',
+      list:['a','b','c']});
 });
 
 module.exports = router;
